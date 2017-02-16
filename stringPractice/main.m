2f269b0f4fc8068f824e9e10e12108f0c3e5aa1a
praveen <praveen@gmail.com>
2017-02-15 20:42:47 -0500

    appendingName

--- stringPractice/main.m
+++ stringPractice/main.m
@@ -12,6 +12,16 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        NSString *firstName = @"praveen";
+        NSString *lastName = @"chowdary";
+        
+        
+        NSString *appendString = [firstName stringByAppendingString:lastName];
+        
+        
+        NSLog(@"print the full name %@", appendString);
+        
     }
     return 0;
 }
